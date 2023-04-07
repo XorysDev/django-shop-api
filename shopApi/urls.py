d3063bb8d8526016f80f1baef445d95f1a81eeaf
XorysDev <xorysteam.dev@gmail.com>
2023-04-07 11:41:24 +0600

    add swagger

--- shopApi/urls.py
+++ shopApi/urls.py
@@ -1,10 +1,26 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from category.views import CategoryViewSet
 from product.views import ProductViewSet
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API test",
+      default_version='v1',
+      description="test api for blog api py.26",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 
 router = SimpleRouter()
@@ -13,6 +29,9 @@ router.register('products', ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/v1/accounts/', include('account.urls')),
     path('api/v1/', include(router.urls)),
     path('api/v1/orders/', include('order.urls')),
